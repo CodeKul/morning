fc6bb715b4055bdd89c27c62e056d1d88db5d3f2
aniruddha <aniruddha.kudalkar@gmail.com>
2017-07-26 11:27:34 +0530

    layouts: linear , relative and frame layout

---
+++ layouts/src/main/java/com/melayer/layouts/MainActivity.java
@@ -0,0 +1,24 @@
+package com.melayer.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame_layout);
+
+        /*MainActivity main = this;
+        Context context = main;*/
+        Button btn = new Button(this);
+        btn.setText("CodeKul");
+
+        FrameLayout frame = findViewById(R.id.frameDynamic);
+        frame.addView(btn);
+    }
+}
--- viewprops/src/main/java/com/melayer/viewprops/MainActivity.java
+++ viewprops/src/main/java/com/melayer/viewprops/MainActivity.java
@@ -1,23 +1,22 @@
-package com.codekul.dailyapp;
+package com.melayer.viewprops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isVisible = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void onPlane(View view) {
-        ((ImageView)findViewById(R.id.imageView)).setImageResource(R.drawable.ic_airplane);
-    }
-
-    public void onBus(View view) {
-        ((ImageView)findViewById(R.id.imageView)).setImageResource(R.drawable.ic_buses);
+    public void onOkay(View view){
+        if(isVisible)
+            findViewById(R.id.btnOkay).setVisibility(View.GONE);
+        else findViewById(R.id.btnOkay).setVisibility(View.VISIBLE);
+        isVisible = !isVisible;
     }
 }
